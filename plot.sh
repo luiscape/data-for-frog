162e038f2984bb3bb1a5b9d008f988adf35d2c5b
Elliot Winard <enw@caveteen.com>
2014-06-24 10:13:18 -0400

    refactor - now plot.sh generates plots & builds styled HTML page

--- plot.sh
+++ plot.sh
@@ -1,1 +1,36 @@
+#!/bin/sh
+
+# generate images
 Rscript code/plot.R
+
+# make html
+PLOTDIR='plots'
+INDEXFILE='index.html'
+STYLEFILE='style.css'
+HEADER='<!DOCTYPE html>\n
+<html>\n
+<head>\n
+  <meta charset="utf-8">\n
+  <link rel="stylesheet" href="style.css">\n
+</head>\n
+<body>\n
+'
+FOOTER='</body>\n
+</html>\n
+'
+CSS='
+img {\n
+  width:3in;\n
+  height:3in;\n
+}\n
+'
+
+cd $PLOTDIR
+echo $HEADER > $INDEXFILE
+for f in *.png
+do
+  echo "<img src=\"$f\"></img>" >> $INDEXFILE
+done
+echo $FOOTER >> $INDEXFILE
+
+echo $CSS > $STYLEFILE
